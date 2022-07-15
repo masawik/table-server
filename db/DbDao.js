67b0c511f52703e36d7546de7b7c4c0b95dc1057
Rochev Alexandr <velloso3@yandex.ru>
2022-07-15 23:17:26 +0300

    add "all" method

--- db/DbDao.js
+++ db/DbDao.js
@@ -4,6 +4,7 @@ import path from 'path';
 const DATABASE_PATH = `${path.resolve()}/sample.db`;
 console.log(DATABASE_PATH);
 
+//todo вынести обработку ошибок
 class DbDao {
   constructor(dbFilePath = DATABASE_PATH) {
     this.db = new sqlite3.Database(dbFilePath, (err) => {
@@ -30,6 +31,20 @@ class DbDao {
     queries.forEach(async (query) => {
       await this.run(query);
     });
+  }
+
+  all(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(query, params, (err, rows) => {
+        if (err) {
+          console.log('Error running query: ' + query)
+          console.log(err)
+          reject(err)
+        } else {
+          resolve(rows)
+        }
+      })
+    })
   }
 }
 

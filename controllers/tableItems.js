4b513f7fddb16fa06c79adbd3b63e9cd3991ee83
Rochev Alexandr <velloso3@yandex.ru>
2022-07-16 12:02:01 +0300

    remove magic numbers; exclude db entities from getAll function

--- controllers/tableItems.js
+++ controllers/tableItems.js
@@ -1,11 +1,16 @@
 import DbSampleTableData from '../db/DbSampleTableData.js';
 import DbDao from '../db/DbDao.js';
 
-export const getAll = async (req, res) => {
-  const dao = new DbDao();
-  const sampleTableData = new DbSampleTableData(dao);
+const DEFAULT_ROWS_PER_PAGE = 20;
+const MAX_ROWS_PER_PAGE = 40;
 
-  const perPage = req.query['per_page'] ?? 20;
+const dao = new DbDao();
+const sampleTableData = new DbSampleTableData(dao);
+
+export const getAll = async (req, res) => {
+  const clientPerPage = req.query['per_page'] ?? DEFAULT_ROWS_PER_PAGE;
+  const perPage = Math.max(clientPerPage, MAX_ROWS_PER_PAGE);
+
   const page = (req.query['page'] ?? 0) * perPage;
   const sortBy = req.query['sort_key'];
 

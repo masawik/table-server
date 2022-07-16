b3f3d5cf5908d3e225f454b724033fcb942cbf2d
Rochev Alexandr <velloso3@yandex.ru>
2022-07-16 18:47:57 +0300

    prepare to deploy

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const tableItemsRouter = require('./routes/tableItems.js');
 
 const PORT = process.env.PORT ?? 3010;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, './build')));
 app.use(tableItemsRouter);
 
 app.listen(PORT, () => {
